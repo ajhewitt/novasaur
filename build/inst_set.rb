b28ae6926829fde192ab9ecfe3dc5f142d272e47
ajhewitt <al@radiusnetworks.com>
2019-09-14 16:43:02 -0400

    updated instrucion encoding

--- build/inst_set.rb
+++ build/inst_set.rb
@@ -1,11 +1,11 @@
-DST = %w[N E1 E2 E3 E4 E5 E6 E7 X E SC V Y HL PC PG]
-SRC = %w[I E IZ EZ I E IZ EZ A D0 AZ D0Z A D1 AZ D1Z]
-ALU = %w[MV ADD SUB AS AND OR XOR DEC MUL DIV FNA FNB FNC FND FNE FNF]
+DST = %w[N E1 E2 E3 E4 E5 E6 E7 X E S V Y HL PC PG]
+SRC = %w[I IZ E EZ I IZ E EZ A AZ D1 D1Z A AZ D0 D0Z]
+ALU = %w[MV ADD SUB AS AND OR XOR DPG MUL DIV SYS AUD VID PS2 FNE FNF]
 LD = %w[NOP NOPZ LD LDZ LDP LDPZ LDN LDNZ]
-WR = %w[D0 D1]
+WR = %w[D1 D0]
 
 def decode(i, a); a.each_with_index.map{|b, n| i[b] * (2 ** n)}.reduce(:+) end
-def src(i); SRC[decode i,  [2, 3, 4, 10]] end
+def src(i); SRC[decode i,  [3, 4, 10, 11]] end
 def dst(i); DST[decode i, [0, 1, 8, 9]] end
 def rom(i); "ROM#{(decode i,  [12, 13, 14, 15]).to_s(16).upcase}" end
 
@@ -26,7 +26,7 @@ end
 
 def inst(i)
   dest = dst(i)
-  dest += WR[i[4]] if i[11] == 0
+  dest += WR[i[10]] if i[2] == 0
   case decode i, [5, 6, 7]
   when 0
     "#{alu(i, 'HL', dest)}"
@@ -52,13 +52,13 @@ is = {}
 (2**16).times do |n|
   i = 16.times.each_with_object([]) {|b, a| a[b] = n >> b & 1}
   next unless i[7] == 0
-  is["#{inst(i)}"] ||= [n >> 8, n & 0xff]
+  is["#{inst(i)}"] ||= (n & 0xff).to_s(16).upcase.rjust(2, '0') + (n >> 8).to_s(16).upcase.rjust(2, '0')
 end
 
 (2**7).times do |n|
   i = 7.times.each_with_object([]) {|b, a| a[b] = n >> b & 1}
   i += [1, 1, 1, 1, 1, 1, 1, 1, 1]
-  is[inst(i)] ||= [n + 0x80]
+  is[inst(i)] ||= (n + 0x80).to_s(16).upcase.rjust(2, '0')
 end
 
 require 'json'
