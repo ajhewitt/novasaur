096c3aa0069ea2e09ac06cfcba567b8883f97b54
ajhewitt <al@radiusnetworks.com>
2022-04-02 16:04:02 -0400

    add kernel timers Initial working timers - add timer to heap, garbage collect on timeout. Use to with ctrl-G impl of BEL

--- src/os/kernel.asm
+++ src/os/kernel.asm
@@ -1,20 +1,31 @@
 ; TITLE: 'KERNEL'
 ;
-; JAN 11, 2022
+; APR 2, 2022
 ;
         .PROJECT        kernel.com
 ;
 STACK   EQU     0E880H
 BREAK   EQU     STACK
 ;
-SRCCPU  EQU     STACK+1
-HANDPG  EQU     0E8H    ;HANDLER TABLE E7->EF
+SRCCPU  EQU     BREAK+1
+CSTAT   EQU     SRCCPU+1
+LASTT0  EQU     CSTAT+1
+TTOP    EQU     LASTT0+1
+HANDPG  EQU     0E8H    ;HANDLER TABLE E9->EF
+TIMER   EQU     0E900H
+;
+BEL     EQU     7
+SERIAL  EQU     8
+CONSOLE EQU     9
+AMODE   EQU     0AH
+TIME0   EQU     3CH
 ;
 YIELD	EQU	06EDH   ;MASTER: YIELD UNTIL CTX SW
 SIGNAL  EQU     07DDH   ;MASTER: SIGNAL SLAVE
 IPCSND	EQU	08DDH   ;MASTER: SET SLAVE REGS
 IPCRCV	EQU	09DDH   ;MASTER: GET SLAVE REGS
 RECXFER	EQU	0BEDH   ;MASTER: MOVE RECORD
+DMA     EQU     0DDDH   ;DMA
 ;
         .ORG    0F000H
 ;
@@ -22,9 +33,17 @@ RECXFER	EQU	0BEDH   ;MASTER: MOVE RECORD
 ; SCAN MSG BOXES - EXIT IF MSG RECEIVED
 ;
 
+        LXI     H,TIMER
+        SHLD    TTOP
 START:  LDA     BREAK
         ORA     A       ;CHECK BREAK (A!=0)
         RNZ             ;RETURN ON BREAK
+
+        LXI     H,LASTT0;GET LAST T0
+        IN      TIME0   ;A=T0
+        CMP     M       ;T0==LAST T0?
+        CNZ     TICK    ;T0 CHANGED
+
 WAIT:   DW	YIELD   ;WAIT UNTIL CTX SW
         XRA     A       ;A=0
 SCAN:   INR     A       ;A+1
@@ -38,7 +57,7 @@ SCAN:   INR     A       ;A+1
 ; MSG HANDLER
 ;
 K_CMD:  MOV	A,C	;A=CMD
-        CPI	4       ;LIMIT CMD<4
+        CPI	7       ;LIMIT CMD<7
         JNC     WAIT    ;SKIP HIGH CMD
         CPI     1       ;CHECK RETURN
         JC      WAIT    ;NULL CMD
@@ -128,16 +147,145 @@ PUT:    LDA     SRCCPU
         DW	IPCSND	;SEND PUT
 	JMP	WAIT
 ;
+; TTY STATUS
+; E=0 IF READY, ELSE 1
+;
+TTYS:   MVI     E,0     ;ASSUME READY
+        LDA     CSTAT   ;LOAD LAST STATUS
+        ORA     A
+        JNZ     SRDY    ;STATUS READY
+        IN      CONSOLE ;READ CHAR
+        STA     CSTAT   ;STORE CHAR
+        ORA     A
+        JNZ     SRDY    ;STATUS READY
+        INR     E       ;NOT READY A=1
+SRDY:   LDA     SRCCPU
+        DW	IPCSND	;SEND TTY STATUS
+        JMP     WAIT
+;
+; TTY INPUT
+; console -> E
+;
+TTYI:   LDA     CSTAT   ;LOAD LAST STATUS
+        ORA     A
+        JNZ     IRDY    ;INPUT READY
+        IN      CONSOLE ;CHAR IN
+IRDY:   CPI     BEL     ;BELL CHAR?
+        CZ      DING
+        MOV     E,A     ;E=CHAR
+        XRA     A       ;A=0
+        STA     CSTAT   ;CLEAR STATUS
+        LDA     SRCCPU
+        DW      IPCSND
+        JMP     WAIT
+;
+; TTY OUT
+; E -> console
+;
+TTYO:   MOV     A,E
+        OUT     CONSOLE ;CHAR OUT
+        MOV     E,A     ;E=0 IF EOL
+        LDA     SRCCPU
+        DW      IPCSND
+        JMP     WAIT
+;
+; TICK - SCAN TIMER HEAP
+; [COUNT,CALL ADDR]
+; TIMED OUT AT COUNT ZERO
+;
+TICK:   STA     LASTT0  ;SAVE T0
+        LXI     H,TIMER
+        PUSH    H
+TICKR:  POP     H
+TICK1:  XCHG            ;SAVE HL, DE=CURT
+        LXI     H,TTOP  ;HL=TTOP
+        MOV     A, M
+        CMP     E
+        JNZ     TICK2   ;CURT!-TTOP
+        INX     H
+        MOV     A, M
+        CMP     D
+        RZ              ;CURT==TTOP
+TICK2:  XCHG            ;HL=CURT
+        DCR     M       ;COUNT-1
+        JZ      TFIRE   ;TIMED OUT
+        LXI     B, 3
+        DAD     B       ;CURT+3
+        JMP     TICK1
+TFIRE:  PUSH    H       ;SAVE CURT
+        LXI     D,TICKR
+        PUSH    D       ;PUSH RETURN
+        INX     H
+        MOV     E, M    ;E=LSB
+        INX     H
+        MOV     D, M    ;D=MSB
+        PUSH    D       ;PUSH CALL
+        LXI     B, -3
+        DAD     B       ;HL=CURT-1
+        XCHG            ;SAVE HL
+        LHLD    TTOP
+        DAD     B       ;HL=TTOP-3
+        SHLD    TTOP
+        DCX     H
+        XCHG
+        MVI     C, 3
+        DW      DMA     ;COPY TOP TIMER TO CUR
+        RET
+;
+; TIMER ADD
+; C=COUNT, DE=CALL ADDR
+;
+TADD:   LHLD    TTOP
+        MOV     M, C
+        INX     H
+        MOV     M, E
+        INX     H
+        MOV     M, D
+        INX     H
+        SHLD    TTOP
+        RET
+;
+DING:   MVI     A, 1
+        OUT     AMODE
+        MVI     A, 2    ; wave:0=sine,1=saw,2=sqr,15=noise
+        LXI     B, 00C02H; attack,decay
+        LXI     D, 00C02H; sustain, release
+        DW      021DDH  ; config voice 1
+        MVI     A, 48H  ; MIDI note number
+        DW      029DDH  ; set note 1
+        DW      02DDDH  ; gate note on
+        MVI     C, 2
+        LXI     D, DONG
+        CALL    TADD
+        MVI     C, 8
+        LXI     D, AUDOFF
+        CALL    TADD
+        MVI     A,BEL
+        RET
+; GATE OFF
+DONG:   DW      025DDH  ; gate note off
+        RET
+; AUDIO OFF
+AUDOFF: XRA     A
+        OUT     AMODE
+        RET
+;
 ; COMMAND JUMP VECTOR TABLE
 ;
 CMDS:   DW      WAIT    ;NULL N/A
         DW      WAIT    ;RETURN N/A
-        DW      GET
-        DW      PUT
+        DW      GET     ;GET RECORD
+        DW      PUT     ;PUT RECORD
+        DW      TTYS
+        DW      TTYI
+        DW      TTYO
 ;
 ; RETURN JUMP VECTOR TABLE
 ;
 RETS:   DW      WAIT    ;NULL N/A
         DW      WAIT    ;RETURN N/A
         DW      GETR
+        DW      WAIT
+        DW      WAIT
+        DW      WAIT
         DW      WAIT
